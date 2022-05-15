ff46f1111094068b9c4a8844fdac03cdd4d2938d
Manar-Alzoubi <mzzo3bi20988@gmail.com>
2022-05-16 00:19:15 +0300

    lab26 solved

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # taskmaster1
+
+* the task master app has three pages:
+* Main page (My Tasks contains: title "My Task", picture, 2 buttons(add task, all tasks))
+
+
+
+![main activity page](screenShots/main.jpg)
+
+
+
+
+* add Task page (contains title, and some data to write: task title and description, button to submit and  text: total tasks)
+
+
+
+
+![add task page](screenShots/add.jpg)
+
+
+
+* all tasks page (contains image and back button)
+
+
+
+![all tasks page](screenShots/all.jpg)
---
+++ app/src/main/java/com/example/taskmaster1/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.taskmaster1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addTask = findViewById(R.id.button4);
+        addTask.setOnClickListener(view -> {
+            Intent addTaskActivity = new Intent(this, addTask.class);
+            startActivity(addTaskActivity);
+        });
+
+        Button allTask = findViewById(R.id.button5);
+        allTask.setOnClickListener(view -> {
+            Intent allTaskActivity = new Intent(this, allTasks.class);
+            startActivity(allTaskActivity);
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: called");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "onResume: called - The App is VISIBLE");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "onPause: called");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(TAG, "onStop: called");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(TAG, "onDestroy: called");
+        super.onDestroy();
+    }
+}
